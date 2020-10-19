965529c1a56a71cf1b99beadd2605e46511283a9
Roshan <nahsorad@gmail.com>
2020-10-19 17:18:22 +0545

    Initial commit

---
+++ src/test/java/np/com/roshanadhikary/springjpa/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.roshanadhikary.springjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
